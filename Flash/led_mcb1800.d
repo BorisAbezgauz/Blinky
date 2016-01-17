30ad645fdf74cbf624b0d4578082f7858a8b4980
BAbezgauz <abez4@yandex.ru>
2016-01-17 16:09:35 +0400

    Version 1

--- Flash/led_mcb1800.d
+++
@@ -1,5 +0,0 @@
-.\flash\led_mcb1800.o: C:\Keil_v5\ARM\PACK\Keil\LPC1800_DFP\2.5.0\Boards\Keil\MCB1800\Common\LED_MCB1800.c
-.\flash\led_mcb1800.o: C:\Keil_v5\ARM\PACK\Keil\LPC1800_DFP\2.5.0\RTE_Driver\SCU_LPC18xx.h
-.\flash\led_mcb1800.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
-.\flash\led_mcb1800.o: C:\Keil_v5\ARM\PACK\Keil\LPC1800_DFP\2.5.0\RTE_Driver\GPIO_LPC18xx.h
-.\flash\led_mcb1800.o: C:\Keil_v5\ARM\PACK\Keil\MDK-Middleware\6.4.0\Board\Board_LED.h
--- Flash/system_lpc18xx.d
+++
@@ -1,8 +0,0 @@
-.\flash\system_lpc18xx.o: RTE\Device\LPC1857\system_LPC18xx.c
-.\flash\system_lpc18xx.o: C:\Keil_v5\ARM\PACK\Keil\LPC1800_DFP\2.5.0\Device\Include\LPC18xx.h
-.\flash\system_lpc18xx.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\4.5.0\CMSIS\Include\core_cm3.h
-.\flash\system_lpc18xx.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
-.\flash\system_lpc18xx.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\4.5.0\CMSIS\Include\core_cmInstr.h
-.\flash\system_lpc18xx.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\4.5.0\CMSIS\Include\cmsis_armcc.h
-.\flash\system_lpc18xx.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\4.5.0\CMSIS\Include\core_cmFunc.h
-.\flash\system_lpc18xx.o: C:\Keil_v5\ARM\PACK\Keil\LPC1800_DFP\2.5.0\Device\Include\system_LPC18xx.h
